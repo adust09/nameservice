0c9acbb4acebcfd59cd3aef2674eef08ed67ea83
ts 21 <shoukitsuda0310@gmail.com>
2021-03-09 21:02:43 +0900

    Genesis

--- x/nameservice/genesis.go
+++ x/nameservice/genesis.go
@@ -9,14 +9,24 @@ import (
 
 // InitGenesis initialize default parameters
 // and the keeper's address to pubkey map
-func InitGenesis(ctx sdk.Context, k keeper.Keeper /* TODO: Define what keepers the module needs */, data types.GenesisState) {
+func InitGenesis(ctx sdk.Context, k keeper.Keeper , data types.GenesisState) {
 	// TODO: Define logic for when you would like to initalize a new genesis
+	for _, record := range data.WhoisRecord{
+		keeper.SetWhois(ctx, record.Value,record)
+	}
 }
 
 // ExportGenesis writes the current store values
 // to a genesis file, which can be imported again
 // with InitGenesis
-func ExportGenesis(ctx sdk.Context, k keeper.Keeper) (data types.GenesisState) {
+func ExportGenesis(ctx sdk.Context, k keeper.Keeper) types.GenesisState {
 	// TODO: Define logic for exporting state
-	return types.NewGenesisState()
+	var record []types.whois
+	iterator := k.GetNameIterator(ctx)
+	for ; iterator.Valid(); iterator.Next(){
+		name := string(iterator.key())
+		whois, _ := k.GetWhois(ctx,name)
+		record = append(record,whois)
+	}
+	return types.GenesisState{WhoisRecords: records}
 }
--- x/nameservice/types/genesis.go
+++ x/nameservice/types/genesis.go
@@ -3,12 +3,14 @@ package types
 // GenesisState - all nameservice state that must be provided at genesis
 type GenesisState struct {
 	// TODO: Fill out what is needed by the module for genesis
+	whoisRecords []whois `json:"whois_records"`
 }
 
 // NewGenesisState creates a new GenesisState object
 func NewGenesisState( /* TODO: Fill out with what is needed for genesis state */ ) GenesisState {
 	return GenesisState{
 		// TODO: Fill out according to your genesis state
+		whoisRecords: nil,
 	}
 }
 
@@ -16,11 +18,23 @@ func NewGenesisState( /* TODO: Fill out with what is needed for genesis state */
 func DefaultGenesisState() GenesisState {
 	return GenesisState{
 		// TODO: Fill out according to your genesis state, these values will be initialized but empty
+		whoisRecords: []Whois{},
 	}
 }
 
 // ValidateGenesis validates the nameservice genesis parameters
 func ValidateGenesis(data GenesisState) error {
 	// TODO: Create a sanity check to make sure the state conforms to the modules needs
+	for _, record := range data.WhoisRecords {
+		if record.Creator == nil {
+			return fmt.Errorf("invalid WhoisRecord: Creator: %s. Error: Missing Creator", record.Creator)
+		}
+		if record.Value == "" {
+			return fmt.Errorf("invalid WhoisRecord: Value: %s. Error: Missing Value", record.Value)
+		}
+		if record.Price == nil {
+			return fmt.Errorf("invalid WhoisRecord: Price: %s. Error: Missing Price", record.Price)
+		}
+	}
 	return nil
 }
