aecb4f5732ac38477b890171da5280896fcae0b2
ts 21 <shoukitsuda0310@gmail.com>
2021-03-09 21:50:44 +0900

    Entry Point

--- x/nameservice/types/querier.go
+++ x/nameservice/types/querier.go
@@ -2,24 +2,24 @@ package types
 
 import "strings"
 
-	const QueryListWhois = "list-whois"
-	const QueryGetWhois = "get-whois"
-	const QueryResolveName = "resolve-name"
+const QueryListWhois = "list-whois"
+const QueryGetWhois = "get-whois"
+const QueryResolveName = "resolve-name"
 
-	//QueryResolve Queries Result Payload fo a resolve query
-	type QueryResResolve struct {
-		Value struct `json: "value"`
-	}
+// QueryResResolve Queries Result Payload for a resolve query
+type QueryResResolve struct {
+	Value string `json:"value"`
+}
 
-	//implement fmt.Stringer
-func(r QueryResResolve)String() string{
+// implement fmt.Stringer
+func (r QueryResResolve) String() string {
 	return r.Value
 }
 
-//QueryResNames Queries Result Payload for a names query
+// QueryResNames Queries Result Payload for a names query
 type QueryResNames []string
 
-//implemetn fmt.Stringer
-func(n QueryResNames) String() string{
-	return strings.Join(n[:],"\n")
+// implement fmt.Stringer
+func (n QueryResNames) String() string {
+	return strings.Join(n[:], "\n")
 }
