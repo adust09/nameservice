6cb713d4a8bd227ac38a0cf70762604535a865e3
ts 21 <shoukitsuda0310@gmail.com>
2021-03-09 22:07:45 +0900

    コードを修正

--- x/nameservice/genesis.go
+++ x/nameservice/genesis.go
@@ -1,41 +1,32 @@
-package types
+package nameservice
 
-import "fmt"
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/user/nameservice/x/nameservice/keeper"
+	"github.com/user/nameservice/x/nameservice/types"
+	// abci "github.com/tendermint/tendermint/abci/types"
+)
 
-// GenesisState - all nameservice state that must be provided at genesis
-type GenesisState struct {
-	// TODO: Fill out what is needed by the module for genesis
-	WhoisRecords []Whois `json:"whois_records"`
-}
-
-// NewGenesisState creates a new GenesisState object
-func NewGenesisState( /* TODO: Fill out with what is needed for genesis state */ ) GenesisState {
-	return GenesisState{
-		// TODO: Fill out according to your genesis state
-		WhoisRecords: nil,
-	}
-}
-
-// DefaultGenesisState - default GenesisState used by Cosmos Hub
-func DefaultGenesisState() GenesisState {
-	return GenesisState{
-		WhoisRecords: []Whois{},
-	}
-}
-
-// ValidateGenesis validates the nameservice genesis parameters
-func ValidateGenesis(data GenesisState) error {
-	// TODO: Create a sanity check to make sure the state conforms to the modules needs
+// InitGenesis initialize default parameters
+// and the keeper's address to pubkey map
+func InitGenesis(ctx sdk.Context, keeper keeper.Keeper, data types.GenesisState) {
 	for _, record := range data.WhoisRecords {
-		if record.Creator == nil {
-			return fmt.Errorf("invalid WhoisRecord: Creator: %s. Error: Missing Creator", record.Creator)
-		}
-		if record.Value == "" {
-			return fmt.Errorf("invalid WhoisRecord: Value: %s. Error: Missing Value", record.Value)
-		}
-		if record.Price == nil {
-			return fmt.Errorf("invalid WhoisRecord: Price: %s. Error: Missing Price", record.Price)
-		}
+		keeper.SetWhois(ctx, record.Value, record)
 	}
-	return nil
+}
+
+// ExportGenesis writes the current store values
+// to a genesis file, which can be imported again
+// with InitGenesis
+func ExportGenesis(ctx sdk.Context, k keeper.Keeper) types.GenesisState {
+	var records []types.Whois
+	iterator := k.GetNamesIterator(ctx)
+	for ; iterator.Valid(); iterator.Next() {
+
+		name := string(iterator.Key())
+		whois, _ := k.GetWhois(ctx, name)
+		records = append(records, whois)
+
+	}
+	return types.GenesisState{WhoisRecords: records}
 }
--- x/nameservice/types/TypeWhois.go
+++ x/nameservice/types/TypeWhois.go
@@ -4,14 +4,17 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
+var MinNamePrice = sdk.Coins{sdk.NewInt64Coin("nametoken", 1)}
+
 type Whois struct {
 	Creator sdk.AccAddress `json:"creator" yaml:"creator"`
 	ID      string         `json:"id" yaml:"id"`
-    Value string `json:"value" yaml:"value"`
-    Price string `json:"price" yaml:"price"`
+	Value   string         `json:"value" yaml:"value"`
+	Price   sdk.Coins      `json:"price" yaml:"price"`
 }
 
-func NewWhois(){
+// NewWhois returns a new Whois with the minprice as the price
+func NewWhois() Whois {
 	return Whois{
 		Price: MinNamePrice,
 	}
